60eb856facb843a0d25608b61ca2f2a6ed1ff666
zetizom <guddhks1234@gmail.com>
2025-08-25 14:14:51 +0900

    advent of code 연습

---
+++ src/advent of code/1일차/advent of code_1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Sample {
+    public static void main(String[] args) throws IOException {
+        InputStream in = System.in;
+
+        int a;
+        a = in.read();
+
+        System.out.println(a);
+    }
+}
---
+++ src/playlist/PlayList.java
@@ -0,0 +1,4 @@
+package playlist;
+
+public class PlayList {
+}
